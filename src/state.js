f727ffceb70b0689869465f468a653c85dcb54c6
Nevar530 <63804016+Nevar530@users.noreply.github.com>
2026-03-30 12:21:28 -0500

    Enhance createState function with default parameters

--- src/state.js
+++ src/state.js
@@ -1,11 +1,20 @@
-export function createState({ map, mechs, rotation = 0 }) {
+export function createState({
+  map,
+  mechs = [],
+  rotation = 0,
+  content = { mechs: [], weapons: [], sigils: [] }
+}) {
   return {
     map,
     mechs,
     rotation,
-    handlers: {
-      raiseTile: null,
-      lowerTile: null
+    content,
+    selection: {
+      mechId: null,
+      tile: null
+    },
+    hover: {
+      tile: null
     }
   };
 }
