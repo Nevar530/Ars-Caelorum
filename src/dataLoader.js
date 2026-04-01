047ddceeee72d768773d69a02405d664c28cd674
Nevar530 <63804016+Nevar530@users.noreply.github.com>
2026-04-01 12:56:07 -0500

    Update dataLoader.js

--- src/dataLoader.js
+++ src/dataLoader.js
@@ -1,14 +1,16 @@
 export async function loadGameData() {
-  const [mechs, weapons, sigils] = await Promise.all([
+  const [mechs, weapons, sigils, attacks] = await Promise.all([
     loadJson("./data/mechs.json"),
     loadJson("./data/weapons.json"),
-    loadJson("./data/sigils.json")
+    loadJson("./data/sigils.json"),
+    loadJson("./data/attacks.json")
   ]);
 
   return {
     mechs,
     weapons,
-    sigils
+    sigils,
+    attacks
   };
 }
 
