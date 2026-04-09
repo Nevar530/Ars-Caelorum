0199d1aca16ad3416f1aa5de7985db64969ffd5f
Nevar530 <63804016+Nevar530@users.noreply.github.com>
2026-04-09 09:57:56 -0500

    Create renderCore.js

---
+++ src/render/renderCore.js
@@ -0,0 +1,125 @@
+// src/render/renderCore.js
+
+import { MAP_CONFIG, RENDER_CONFIG } from "../config.js";
+import { getTile } from "../map.js";
+import { getReachableTiles } from "../movement.js";
+import { renderTerrainTile, renderEditorTile } from "./renderTerrain.js";
+import { drawMech } from "./renderUnits.js";
+import {
+  drawSceneMoveOverlay,
+  drawScenePathOverlayForTile,
+  drawSceneActionOverlayForTile,
+  drawSceneFocusOverlayForTile
+} from "./renderOverlays.js";
+import {
+  ensureCameraState,
+  updateCameraFraming,
+  projectScene,
+  getSceneSortKey
+} from "./projection.js";
+import { drawSceneLosPreview } from "./renderLosOverlay.js";
+
+export function renderAll(state, refs) {
+  ensureCameraState(state);
+  updateCameraFraming(state, refs);
+  renderIso(state, refs);
+  renderEditor(state, refs);
+}
+
+export function renderIso(state, refs) {
+  const { worldScene, worldUi } = refs;
+  const { map, mechs } = state;
+
+  worldScene.innerHTML = "";
+  worldUi.innerHTML = "";
+
+  const sceneItems = [];
+  const reachableMap = new Map();
+
+  if (state.ui.mode === "move") {
+    for (const tile of getReachableTiles(state)) {
+      reachableMap.set(`${tile.x},${tile.y}`, tile.cost);
+    }
+  }
+
+  for (let y = 0; y < MAP_CONFIG.mechHeight; y++) {
+    for (let x = 0; x < MAP_CONFIG.mechWidth; x++) {
+      const tile = getTile(map, x, y);
+      const projected = projectScene(state, x, y, tile.elevation);
+
+      sceneItems.push({
+        kind: "tile",
+        x,
+        y,
+        elevation: tile.elevation,
+        screenX: projected.x,
+        screenY: projected.y,
+        sortKey: getSceneSortKey(state, x, y, tile.elevation),
+        reachableCost: reachableMap.get(`${x},${y}`) ?? null
+      });
+    }
+  }
+
+  for (const mech of mechs) {
+    const tile = getTile(map, mech.x, mech.y);
+    if (!tile) continue;
+
+    const projected = projectScene(state, mech.x, mech.y, tile.elevation);
+
+    sceneItems.push({
+      kind: "mech",
+      mech,
+      elevation: tile.elevation,
+      screenX: projected.x,
+      screenY: projected.y,
+      sortKey: getSceneSortKey(state, mech.x, mech.y, tile.elevation) + 0.25
+    });
+  }
+
+  sceneItems.sort((a, b) => a.sortKey - b.sortKey);
+
+  for (const item of sceneItems) {
+    if (item.kind === "tile") {
+      renderTerrainTile(state, item, worldScene);
+
+      if (state.ui.mode === "move" && item.reachableCost !== null) {
+        drawSceneMoveOverlay(state, item, worldScene, String(item.reachableCost));
+      }
+
+      drawScenePathOverlayForTile(state, item, worldScene);
+      drawSceneActionOverlayForTile(state, item, worldScene);
+      drawSceneFocusOverlayForTile(state, item, worldScene);
+    } else {
+      const isActive = item.mech.instanceId === state.turn.activeMechId;
+      drawMech(state, item.mech, item.screenX, item.screenY, worldScene, isActive);
+    }
+  }
+
+  drawSceneLosPreview(state, worldUi);
+}
+
+export function renderEditor(state, refs) {
+  const { editor } = refs;
+  const { map } = state;
+
+  editor.innerHTML = "";
+
+  const usable = RENDER_CONFIG.editorSize - (RENDER_CONFIG.editorPadding * 2);
+  const cellWidth = usable / MAP_CONFIG.mechWidth;
+  const cellHeight = usable / MAP_CONFIG.mechHeight;
+
+  for (let y = 0; y < MAP_CONFIG.mechHeight; y++) {
+    for (let x = 0; x < MAP_CONFIG.mechWidth; x++) {
+      const tile = getTile(map, x, y);
+      renderEditorTile(tile, x, y, px(x, cellWidth), py(y, cellHeight), cellWidth, cellHeight, editor);
+    }
+  }
+
+  function px(x, cellWidthValue) {
+    return RENDER_CONFIG.editorPadding + (x * cellWidthValue);
+  }
+
+  function py(y, cellHeightValue) {
+    return RENDER_CONFIG.editorPadding + (y * cellHeightValue);
+  }
+}
